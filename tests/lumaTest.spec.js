6c4a581646431519b57de4fdb8a5b40ffe82f846
MariaMoroz <82988399+MariaMoroz@users.noreply.github.com>
2024-04-07 19:24:42 -0400

    first test (#1)
    * first test

---
+++ tests/lumaTest.spec.js
@@ -0,0 +1,22 @@
+import { test, expect } from "@playwright/test";
+
+test.describe('luma test', () => {
+  const BASE_URL = "https://magento.softwaretestingboard.com";
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+  })
+
+  test('verify page title', async ({ page }) => {
+
+    await expect(page).toHaveTitle('Home Page');
+  })
+
+  test('verify user can navigate to home page clicking on logo', async ({ page }) => {
+    await page.getByRole('listitem').filter({ hasText: "What's New" }).click();
+    await expect(page).toHaveURL(BASE_URL + '/what-is-new.html');
+
+    await page.getByLabel('store logo').click();
+    await expect(page).toHaveURL(BASE_URL);
+  })
+})
