0761d60b5f7c38f71b2369903593099cebd452f1
Babintseva Svetlana <83321269+BabintsevaS@users.noreply.github.com>
2024-04-13 16:08:15 -0500

    TC 02.2.1_01 User is redirected to Privacy Policy page (#11)
    * TC 02.2.1_01 User is redirected to Privacy Policy page

--- tests/footer.spec.js
+++ tests/footer.spec.js
@@ -1,18 +1,27 @@
 import { test, expect } from "@playwright/test";
 
-test.describe('footer', () => {
-    const NOTES_URL = 'https://softwaretestingboard.com/magento-store-notes/?utm_source=magento_store&utm_medium=banner&utm_campaign=notes_promo&utm_id=notes_promotion';
-  
-    test.beforeEach(async ({ page }) => {
-      await page.goto('/');
-    })
+test.describe("footer", () => {
+  const NOTES_URL =
+    "https://softwaretestingboard.com/magento-store-notes/?utm_source=magento_store&utm_medium=banner&utm_campaign=notes_promo&utm_id=notes_promotion";
+  const POLICY_URL =
+    "https://magento.softwaretestingboard.com/privacy-policy-cookie-restriction-mode";
 
-    test('user is redirected to Notes page', async({ page }) => {
-      const pagePromise = page.waitForEvent('popup');
-      
-      await page.getByRole('link', {name: 'Notes'}).click();
-      const pageNotes = await pagePromise;
-        
-      await expect(pageNotes).toHaveURL(NOTES_URL);      
-      })
-})    
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/");
+  });
+
+  test("user is redirected to Notes page", async ({ page }) => {
+    const pagePromise = page.waitForEvent("popup");
+
+    await page.getByRole("link", { name: "Notes" }).click();
+    const pageNotes = await pagePromise;
+
+    await expect(pageNotes).toHaveURL(NOTES_URL);
+  });
+
+  test("user is redirected to Privacy Policy page", async ({ page }) => {
+    await page.getByRole("link", { name: "Privacy and Cookie Policy" }).click();
+
+    await expect(page).toHaveURL(POLICY_URL);
+  });
+});
