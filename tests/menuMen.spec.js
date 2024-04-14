0a0fb58a498230301d6f11e8e988719fe85bf211
Svetlana <153175451+CSvetLana@users.noreply.github.com>
2024-04-14 21:52:44 +0300

    TC 04.3.1_01 Verify navigation from the menu to page Men (#21)
    Co-authored-by: Vera Fesianava <verafes@gmail.com>

---
+++ tests/menuMen.spec.js
@@ -0,0 +1,15 @@
+import{test, expect} from '@playwright/test';
+
+test.describe ('MenMenu', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto("/");
+    })
+
+    test ('Verify navigation from the menu to page Men', async ({page}) => {
+        await page.getByRole('menuitem', { name: 'Men' }).last().click();
+      
+        await expect(page).toHaveURL('https://magento.softwaretestingboard.com/men.html');
+        await expect(page.getByRole('heading', { name: 'Men'})).toBeVisible();
+    })
+
+})
