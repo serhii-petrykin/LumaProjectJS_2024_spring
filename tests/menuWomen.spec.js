b22ea94c5aff0b42e973930b42564a0653949091
Babintseva Svetlana <83321269+BabintsevaS@users.noreply.github.com>
2024-04-14 14:19:34 -0500

    TC 04.2.1_03 <Menu/Women> Women>Tops dropdown has items: Jackets, Hoodies & Sweatshirts, Tees, Bras & Tanks (#26)

--- tests/menuWomen.spec.js
+++ tests/menuWomen.spec.js
@@ -12,4 +12,22 @@ test.describe("menuWomen", () => {
     await expect(page.locator('#ui-id-9')).toBeVisible();
     await expect(page.locator('#ui-id-10')).toBeVisible();  
   })
+
+  test("Women>Tops dropdown has items: Jackets, Hoodies & Sweatshirts, Tees, Bras & Tanks", async ({ page }) => {
+    await page.getByText('Women').hover();
+    await page.locator('#ui-id-9').hover();
+
+    await expect(page.locator('#ui-id-11')).toBeVisible();
+    await expect(page.locator('#ui-id-11')).toHaveText('Jackets');
+
+    await expect(page.locator('#ui-id-12')).toBeVisible();
+    await expect(page.locator('#ui-id-12')).toHaveText('Hoodies & Sweatshirts');
+
+    await expect(page.locator('#ui-id-13')).toBeVisible();
+    await expect(page.locator('#ui-id-13')).toHaveText('Tees');
+    
+    await expect(page.locator('#ui-id-14')).toBeVisible();
+    await expect(page.locator('#ui-id-14')).toHaveText('Bras & Tanks');  
+  })
+
 })  
