4cbf5360ed6d9c28e4be32a58b7b4993cd67c1f8
Сергей <102795971+trukhachev495@users.noreply.github.com>
2024-04-15 11:42:12 +0300

    TC 06.2.1_01 Selection the men's bottom section (#27)

--- tests/menuMen.spec.js
+++ tests/menuMen.spec.js
@@ -1,15 +1,22 @@
-import{test, expect} from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-test.describe ('MenMenu', () => {
-    test.beforeEach(async ({ page }) => {
-        await page.goto("/");
-    })
+test.describe('menuMen', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+  })
+  test("Selection the men's bottom section", async ({ page }) => {
+    page.getByRole('menuitem', { name: ' Men' }).hover();
+    page.locator('#ui-id-18').click();
 
-    test ('Verify navigation from the menu to page Men', async ({page}) => {
+    await expect(page.locator('//span[@data-ui-id="page-title-wrapper" and text()= "Bottoms"]') ).toHaveText('Bottoms');
+    await expect(page).toHaveURL( 'https://magento.softwaretestingboard.com/men/bottoms-men.html');
+  })
+   test ('Verify navigation from the menu to page Men', async ({page}) => {
         await page.getByRole('menuitem', { name: 'Men' }).last().click();
       
         await expect(page).toHaveURL('https://magento.softwaretestingboard.com/men.html');
         await expect(page.getByRole('heading', { name: 'Men'})).toBeVisible();
     })
-
 })
+
+
