15c5d6fc8b1f9c21dc95427cb8756b0f89c48198
annanazcz <166229563+annanazcz@users.noreply.github.com>
2024-04-15 22:47:02 +0200

    TC 04.4.3_04 Redirect to the "Fitness Equipment" category from "Shop Equipment" banner (#58)

--- tests/menuGearPromoBlocks.spec.js
+++ tests/menuGearPromoBlocks.spec.js
@@ -8,7 +8,7 @@ test.describe('Menu/Gear/Promo Block', () => {
     await gearNavButton.click();
   })
 
-  test('TC 04.4.3_01 Verify redirect to the "Fitness Equipment" page by clicking on "Loosen Up" block', async ({ page }) => {
+  test('TC 04.4.3_01 Verify redirect to the "Fitness Equipment" category by clicking on "Loosen Up" banner', async ({ page }) => {
 
     await page.getByRole('link', { name: 'Loosen Up' }).click();
 
@@ -16,7 +16,7 @@ test.describe('Menu/Gear/Promo Block', () => {
     await expect(page.locator('.base')).toHaveText('Fitness Equipment');
   })
 
-  test('TC 04.4.3_02 Verify redirect to the "Fitness Equipment" page by clicking on “$4 Luma water bottle. Promo code H2O” block', async ({ page }) => {
+  test('TC 04.4.3_02 Verify redirect to the "Fitness Equipment" category by clicking on “$4 Luma water bottle” banner', async ({ page }) => {
 
     await page.locator('.block-promo.gear-equipment').click();
 
@@ -24,12 +24,20 @@ test.describe('Menu/Gear/Promo Block', () => {
     await expect(page.locator('.base')).toHaveText('Fitness Equipment');
   })
   
-  test('TC 04.4.3_03 Verify redirect to the "Bags" category by clicking on "Shop bags" block', async ({ page }) => {
+  test('TC 04.4.3_03 Verify redirect to the "Bags" category by clicking on "Shop bags" banner', async ({ page }) => {
     
     await page.locator('.block-promo.gear-category-bags').click();
     
     await expect(page).toHaveURL(/.*bags.html/);
     await expect(page.locator('.base')).toHaveText('Bags');
+  })
+
+  test('TC 04.4.3_04 Verify redirect to the "Fitness Equipment" category by clicking on "Shop Equipment" banner', async ({ page }) => {
+    
+    await page.locator('.block-promo.gear-category-equipment').click();
+    
+    await expect(page).toHaveURL(/.*fitness-equipment.html/);
+    await expect(page.locator('.base')).toHaveText('Fitness Equipment');
   })
 
 })  
