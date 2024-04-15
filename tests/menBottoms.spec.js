8e6484bc3996c40d608736f0f5f7559f5c612ecf
Сергей <102795971+trukhachev495@users.noreply.github.com>
2024-04-15 17:50:30 +0300

    TC 06.2.1_01 Selection the men's bottom section (#45)
    * TC 06.2.1_01 Selection the men's bottom section
    
    * TC 06.2.1_01 Selection the men's bottom section1
    
    * TC 06.2.1_01 Selection the men's bottom section2

---
+++ tests/menBottoms.spec.js
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('menBottoms', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+  })
+  test("Selection the men's bottom section", async ({ page }) => {
+    page.getByRole('menuitem', { name: ' Men' }).hover();
+    page.locator('#ui-id-18').click();
+
+    await expect(page.locator('//span[@data-ui-id="page-title-wrapper" and text()= "Bottoms"]') ).toHaveText('Bottoms');
+    await expect(page).toHaveURL( 'https://magento.softwaretestingboard.com/men/bottoms-men.html');
+  })
+})
