37cf889b687fb6600b637eba1261c4acaa39cf90
annanazcz <166229563+annanazcz@users.noreply.github.com>
2024-04-15 18:30:13 +0200

    TC 04.4.3_02 Redirect to the "Fitness Equipment" page from “$4 Luma water bottle. Promo code H2O” block (#50)

--- tests/menuGearPromoBlocks.spec.js
+++ tests/menuGearPromoBlocks.spec.js
@@ -4,16 +4,25 @@ test.describe('Menu/Gear/Promo Block', () => {
  
   test.beforeEach(async ({ page }) => {
     await page.goto("/");
-  })
-
-  test('verify redirect to the "Fitness Equipment" page by clicking on "Loosen Up" block', async ({ page }) => {
     const gearNavButton = page.locator('#ui-id-6');
     await gearNavButton.click();
+  })
+
+  test('TC 04.4.3_01 Verify redirect to the "Fitness Equipment" page by clicking on "Loosen Up" block', async ({ page }) => {
 
     await page.getByRole('link', { name: 'Loosen Up' }).click();
 
     await expect(page).toHaveURL(/.*fitness-equipment.html/);
     await expect(page.locator('.base')).toHaveText('Fitness Equipment');
   })
+
+  test('TC 04.4.3_02 Verify redirect to the "Fitness Equipment" page by clicking on “$4 Luma water bottle. Promo code H2O” block', async ({ page }) => {
+
+    await page.locator('.block-promo.gear-equipment').click();
+
+    await expect(page).toHaveURL(/.*fitness-equipment.html/);
+    await expect(page.locator('.base')).toHaveText('Fitness Equipment');
+
+})
 
 })  
