a3eac77de643ae32a11c986dccdb068b03a2c5dd
KarinaKrem <139580950+KarinaKrem@users.noreply.github.com>
2024-04-16 18:20:31 +0300

    TC 04.3.3_01 Redirect to the "Men/Tops/Tees" page from the "Buy 3 Luma tees, get 4 instead" promo block (#81)
    TC 04.3.3_01 Redirect to the "Men/Tops/Tees" page from the "Buy 3 Luma tees, get 4 instead" promo block (#81)

---
+++ tests/menuMenPromoBlock.spec.js
@@ -0,0 +1,16 @@
+import { test, expect } from "@playwright/test";
+test.describe('Menu/Men/Promo Block', () => {
+    const BASE_URL = 'https://magento.softwaretestingboard.com/men'
+    test.beforeEach(async ({ page }) => {
+        await page.goto('/');
+        await page.locator('#ui-id-5').click()
+    })
+
+    test('TC 04.3.3_01 Redirect to the "Men/Tops/Tees" page from the "Buy 3 Luma tees, get 4 instead" promo block', async ({ page }) => {
+
+        await page.getByRole('link', { name: 'Save up to $24!' }).click();
+
+        await expect(page).toHaveURL(BASE_URL + '/tops-men/tees-men.html');
+        await expect(page.getByRole('heading', { name: 'Tees' })).toBeVisible();
+    })
+})
