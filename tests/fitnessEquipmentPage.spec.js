f5472fe61a9d6f83a02283b129a393f4642a6061
Vera Harbachova <79797816+VeraHarbachova@users.noreply.github.com>
2024-04-17 00:48:04 +0200

    TC 07.2.4_01 Gear/Fitnes_Equipment/Display mode (#97)

--- tests/fitnessEquipmentPage.spec.js
+++ tests/fitnessEquipmentPage.spec.js
@@ -12,4 +12,19 @@ test.describe('verify fitness equipment', () => {
         await page.locator('ul > li > a> span:text-is("Fitness Equipment")').click();
         await expect(page.locator('ul.items')).toHaveText('Home Gear Fitness Equipment');
     })
+
+    test('change display mode in the Fitnes Equipment section', async ({ page }) => {
+        await page.goto('/' + 'gear/fitness-equipment.html');
+        
+        if (await page.getByRole('dialog', { name: 'This site asks for consent to use your data' }).isVisible()) {
+            await page.getByRole('button', { name: 'Consent' }).click();
+        };
+        
+        await page.locator('#mode-list').first().click();
+        await expect(page.locator('.toolbar-amount').nth(1)).toHaveText('Items 1-10 of 11');
+        await page.reload();
+
+        await page.locator('#mode-grid').first().click();
+        await expect(page.locator('.toolbar-amount').nth(1)).toHaveText('11 Items');
+    })
 })
