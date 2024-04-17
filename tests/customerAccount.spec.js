18e387c85951a180932e70d2c1768493ca98531c
Pavel Korz <131908496+Paul-Korz@users.noreply.github.com>
2024-04-17 21:27:24 +0300

    TC 11.1.1_02 Verify that clicking on the "Welcome, username" inscription opens the menu (#119)

--- tests/customerAccount.spec.js
+++ tests/customerAccount.spec.js
@@ -1,5 +1,8 @@
 import { test, expect } from "@playwright/test";
+
 test.describe("customerAccount", () => {
+  const BASE_URL = "https://magento.softwaretestingboard.com";
+
   test.beforeEach(async ({ page }) => {
     const firstname = "Angelina-Maria";
     const lastname = "O'Neel";
@@ -8,40 +11,38 @@ test.describe("customerAccount", () => {
       const domain = "example.com";
       return `${mailbox}@${domain}`;
     }
-
     const email = generateRandomEmail();
     const password = "RT45bb%%mm";
 
     await page.goto("/");
     await page.getByRole("link", { name: "Create an Account" }).click();
-    await page.getByRole("textbox", { name: "First Name*" }).click();
     await page.getByRole("textbox", { name: "First Name*" }).fill(firstname);
-
-    await page.getByRole("textbox", { name: "Last Name*" }).click();
     await page.getByRole("textbox", { name: "Last Name*" }).fill(lastname);
-
-    await page.getByRole("textbox", { name: "Email*" }).click();
     await page.getByRole("textbox", { name: "Email*" }).fill(email);
-
-    await page.getByRole("textbox", { name: "Password*", exact: true }).click();
     await page
       .getByRole("textbox", { name: "Password*", exact: true })
       .fill(password);
-
-    await page.getByRole("textbox", { name: "Confirm Password*" }).click();
     await page
       .getByRole("textbox", { name: "Confirm Password*" })
       .fill(password);
-
     await page.getByRole("button", { name: "Create an Account" }).click();
-
     await page.getByRole("link", { name: "store logo" }).click();
   });
+
   test("Customer name is displaied on the home page", async ({ page }) => {
     const greetingMSG = page
       .getByRole("banner")
       .getByText("Welcome, Angelina-Maria O'Neel!");
+      
     await expect(greetingMSG).toBeVisible();
     await expect(greetingMSG).toHaveText("Welcome, Angelina-Maria O'Neel!");
+  });
+
+  test("Verify that clicking on the 'Welcome, username' inscription opens the menu", async ({ page }) => {
+    await page.locator("div[class='panel header'] span[role='button']").click();
+
+    await expect(
+      page.locator("div[aria-hidden='false'] ul[class='header links']")
+    ).toBeVisible();
   });
 });
