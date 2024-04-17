a88dbe6508ac82572ca3d72f531be854c73a8eb3
Babintseva Svetlana <83321269+BabintsevaS@users.noreply.github.com>
2024-04-17 14:07:04 -0500

    TC 05.1.1_02 <Women/Tops> Verify that on the Tops page user sees women's outfits: jackets, hoodies & sweatshirts, tees, bras & tanks (#132)

--- tests/womenTops.spec.js
+++ tests/womenTops.spec.js
@@ -4,6 +4,20 @@ test.describe("womenTops", () => {
   const WOMEN_TOPS_URL =
     "https://magento.softwaretestingboard.com/women/tops-women.html";
 
+  const WOMEN_TOPS_ITEMS = [
+    "Tank",
+    "Tunic",
+    "Top",
+    "Bra",
+    "Tee",
+    "Jacket",
+    "Shell",
+    "Hoodie",
+    "Fleece",
+    "Sweatshirt",
+    "Pullie",
+  ];
+
   test.beforeEach(async ({ page }) => {
     await page.goto("/");
   });
@@ -14,5 +28,22 @@ test.describe("womenTops", () => {
 
     await expect(page).toHaveURL(WOMEN_TOPS_URL);
     await expect(page).toHaveTitle("Tops - Women");
+  });
+
+  test("verify that on the Tops page user sees women's outfits: jackets, hoodies & sweatshirts, tees, bras & tanks", async ({ page }) => {
+    await page.getByText("Women").hover();
+    await page.locator("#ui-id-9").click();
+
+    const allWomenTopsItems = await page
+      .locator(".products .product-items .product-item-link")
+      .allTextContents();
+
+    expect(allWomenTopsItems.length).toBeGreaterThan(0);
+
+    const allItemsContainExpectedText = allWomenTopsItems.every((item) => {
+      return WOMEN_TOPS_ITEMS.some((keyword) => item.includes(keyword));
+    });
+
+    expect(allItemsContainExpectedText).toBeTruthy();
   });
 });
