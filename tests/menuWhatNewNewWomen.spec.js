fb5a9a2687bd125324efe7787f6ea6f4e69e1fea
Katerina Poliit <114866316+Katerina-Poliit@users.noreply.github.com>
2024-04-17 16:07:44 +0000

    TC 04.1.2 _02 Checking the “Hoodies" page & Sweatshirts” (#120)

--- tests/menuWhatNewNewWomen.spec.js
+++ tests/menuWhatNewNewWomen.spec.js
@@ -2,12 +2,14 @@ import { test, expect } from "@playwright/test";
 
 test.describe('page gear', () => {
 
+  const whatNewUrl = 'https://magento.softwaretestingboard.com/what-is-new.html';
     test.beforeEach(async ({ page }) => {
       await page.goto('/');
+
     })
 
     test('should  be "new in women" section', async ({ page }) => {
-        const whatNewUrl = 'https://magento.softwaretestingboard.com/what-is-new.html';
+
         await page.goto(whatNewUrl);
         await expect(page).toHaveURL(whatNewUrl);
         await page.getByText("New in women's");
@@ -16,5 +18,16 @@ test.describe('page gear', () => {
         const newItems =  page.getByText('Hoodies & Sweatshirts Jackets Tees Bras & Tanks Pants Shorts');
         await page.getByText(newItems)
         await expect(newItems).toBeVisible();
+    })
+
+    test('should be a transition to the page "hoodies & sweatshirts"', async ({ page}) => {
+
+      await page.goto(whatNewUrl);
+
+      const hoodiesPage = page.locator('li.item > a[href="https://magento.softwaretestingboard.com/women/tops-women/hoodies-and-sweatshirts-women.html"]');
+
+      await hoodiesPage.click();
+      await expect(page).toHaveURL('https://magento.softwaretestingboard.com/women/tops-women/hoodies-and-sweatshirts-women.html');
+
     })
 })
