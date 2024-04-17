b5eb6b8bfadcd660a4020caa2c35a00b7e57130f
Valeriia Hudzenko <72437450+gudzenkoleruska@users.noreply.github.com>
2024-04-17 06:22:25 -0500

    TC 04.6.4_01 Verify that all links from the “Men’s deal” section open the corresponding page (#68)
    * TC 04.6.4_01 Verify that all links from the “Men’s deal” section open the corresponding page

--- tests/menuSalePromo-block.spec.js
+++ tests/menuSalePromo-block.spec.js
@@ -1,9 +1,17 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("menuSalePromo-block", () => {
+  const links = [
+    { name: 'Hoodies and Sweatshirts', url: 'https://magento.softwaretestingboard.com/men/tops-men/hoodies-and-sweatshirts-men.html', title: 'Hoodies & Sweatshirts' },
+    { name: 'Jackets', url: 'https://magento.softwaretestingboard.com/men/tops-men/jackets-men.html', title: 'Jackets' },
+    { name: 'Tees', url: 'https://magento.softwaretestingboard.com/men/tops-men/tees-men.html', title: 'Tees' },
+    { name: 'Pants', url: 'https://magento.softwaretestingboard.com/men/bottoms-men/pants-men.html', title: 'Pants' },
+    { name: 'Shorts', url: 'https://magento.softwaretestingboard.com/men/bottoms-men/shorts-men.html', title: 'Shorts' }
+  ];
 
   test.beforeEach(async ({ page }) => {
     await page.goto("/" + "sale.html");
+
   });
 
   test("Verify 'Sale' page contains 6 promo-blocks as images", async ({ page }) => {
@@ -11,4 +19,12 @@ test.describe("menuSalePromo-block", () => {
 
     expect(promoBlocks).toHaveCount(6);
   });
+
+  for (const link of links) {
+  test(`Check that ${link.name} link opens the corresponding page`, async ({ page }) => {
+      await page.getByRole('link', { name: link.name }).last().click();
+      await expect(page).toHaveURL(link.url);
+      await expect(page.locator('#page-title-heading > span')).toHaveText(link.title);
+    });
+  }
 })
