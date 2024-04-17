d053a3711c1c174ec9b2c6d3db4c45ca4892c082
KarinaKrem <139580950+KarinaKrem@users.noreply.github.com>
2024-04-17 21:40:11 +0300

    TC 05.2.1_01 Redirection to the Women/Bottoms page after clicking the “Bottoms” droplist category. (#130)

---
+++ tests/womenBottoms.spec.js
@@ -0,0 +1,17 @@
+import { test, expect } from "@playwright/test";
+
+test.describe('Women bottoms page', () => {
+    const BASE_URL = 'https://magento.softwaretestingboard.com';
+
+    test.beforeEach(async ({ page }) => {
+        await page.goto('/');
+    })
+
+    test('TC 05.2.1_01 Redirection to the Women/Bottoms page after clicking the “Bottoms” droplist category.', async ({ page }) => {
+        await page.getByText('Women').hover();
+        await page.locator('#ui-id-10').click();
+
+        await expect(page).toHaveURL(BASE_URL + '/women/bottoms-women.html');
+        await expect(page).toHaveTitle('Bottoms - Women');
+    })
+})
