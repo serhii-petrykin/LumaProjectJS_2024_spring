64ad0a62d8c50788f3fde23194956b1f35d42bae
Svetlana <153175451+CSvetLana@users.noreply.github.com>
2024-04-18 18:32:06 +0300

    TC 04.3.1_03 Verify the display of My Wish List on the men's page (#158)
    * TC 04.3.1_03 Verify the display of My Wish List on the men's page
    
    * modified test

--- tests/menuMen.spec.js
+++ tests/menuMen.spec.js
@@ -13,5 +13,26 @@ test.describe('Menu/Men', () => {
         await expect(page.locator('.page-title')).toContainText('Men');
         await expect(page.locator('[role="heading"]').first()).toBeVisible('Compare Products');
         await expect(page.locator('[role="heading"]').nth(1)).toBeVisible('My Wish List');
-    })
+    })    
+     
+    test("Verify the display of My Wish List on the men's page", async ({ page, }) => {      
+        await page.getByRole("link", { name: "Sign In" }).click();
+        await page.getByLabel("Email").fill("svetik.buratino@gmail.com");
+        await page.getByLabel("Password").fill("User123!");
+        await page.getByRole("button", { name: "Sign In" }).click();
+        await page.getByRole("menuitem", { name: "Men" }).last().click();
+  
+        await page.locator('.products-grid li').first().click();    
+        await page.getByRole('link', {name:'Add to Wish List'}).click();       
+        await page.getByRole("menuitem", { name: "Men" }).last().click();
+        await page.locator('.products-grid li').nth(1).click();
+        await page.getByRole('link', {name:'Add to Wish List'}).click();       
+        await page.getByRole("menuitem", { name: "Men" }).last().click();
+  
+        await expect(page.getByRole('heading', {name:'My Wish List'})).toBeVisible(); 
+  
+        const countItemsBlockWishList = page.locator('#wishlist-sidebar li');
+  
+        await expect(countItemsBlockWishList).toHaveCount(2);  
+    });
 })
