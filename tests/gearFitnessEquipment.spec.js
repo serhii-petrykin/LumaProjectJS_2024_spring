9db204b4e806edfbb6d0152d66f17c5ceb25e5ff
Babintseva Svetlana <83321269+BabintsevaS@users.noreply.github.com>
2024-04-18 13:43:28 -0500

    TC 07.2.1_02 <Gear/Fitnes_Equipment> Verify that on the Fitness Equipment page user sees fitness equipment (#137)

--- tests/gearFitnessEquipment.spec.js
+++ tests/gearFitnessEquipment.spec.js
@@ -2,6 +2,21 @@ import { test, expect } from "@playwright/test";
 
 test.describe('page gear', () => {
 
+  const FITNESS_ITEMS = [
+    "Sprite Yoga Companion Kit",
+    "Set of Sprite Yoga Straps",
+    "Harmony Lumaflex™ Strength Band Kit",
+    "Sprite Foam Roller",
+    "Sprite Foam Yoga Brick",
+    "Quest Lumaflex™ Band",
+    "Go-Get'r Pushup Grips",
+    "Pursuit Lumaflex™ Tone Band",
+    "Zing Jump Rope",
+    "Dual Handle Cardio Ball",
+    "Affirm Water Bottle",
+  ];
+  
+
     test.beforeEach(async ({ page }) => {
       await page.goto('/');
     })
@@ -16,4 +31,21 @@ test.describe('page gear', () => {
         await page.locator('#narrow-by-list2 > dd > ol > li:nth-child(2) > a').click();
         await expect(page).toHaveURL('https://magento.softwaretestingboard.com/gear/fitness-equipment.html')
     })
+
+    test("verify that on the Fitness Equipment page user sees fitness equipment", async ({ page }) => {
+      await page.getByRole('menuitem', { name: 'Gear' }).hover();
+      await page.locator("#ui-id-26").click();
+  
+      const allFitnessItems = await page
+        .locator(".products .product-items .product-item-link")
+        .allTextContents();
+  
+      expect(allFitnessItems.length).toBeGreaterThan(0);
+  
+      const allItemsContainExpectedText = allFitnessItems.every((item) => {
+        return FITNESS_ITEMS.some((keyword) => item.includes(keyword));
+      });
+  
+      expect(allItemsContainExpectedText).toBeTruthy();
+    });
 })
