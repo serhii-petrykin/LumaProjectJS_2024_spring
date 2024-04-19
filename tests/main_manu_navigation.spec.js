1e8dbe422cfd833be9c80562f3a08e201b1c3715
MlimaU <129126676+mlimau@users.noreply.github.com>
2024-04-19 12:14:18 -0400

    test1

--- tests/main_manu_navigation.spec.js
+++ tests/main_manu_navigation.spec.js
@@ -1,1 +1,16 @@
-import { test, expect} from "@playwright/test"; 
+import { test, expect } from '@playwright/test'
+
+test.describe('Main menu navigation', () => {
+    test.beforeEach('Opem main page', async () => {
+        await page.goto('https://magento.softwaretestingboard.com')
+    })
+    
+    test('verify that Main menu is displayed on the top of the home page in the grey field', async ({ page }) => {
+        
+    })
+
+
+    test('verify that user can see menu options on the main page', async ({ page }) => {
+        
+    })
+})
