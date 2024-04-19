90abe36f4fa7a59943d4997031f8d80f25bbadc3
MlimaU <129126676+mlimau@users.noreply.github.com>
2024-04-19 14:28:02 -0400

    tests done

---
+++ tests/main_menu_navigation.spec.js
@@ -0,0 +1,45 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('Main menu navigation', () => {
+    test.beforeEach('Opem main page', async ({page}) => {
+        await page.goto('https://magento.softwaretestingboard.com/')
+    })
+    
+    test('verify Main menu is displayed on the top of the home page in the grey field', async ({ page }) => {
+        const menuBar = await page.locator('.navigation')
+        
+        await expect(menuBar).toBeVisible()
+        await expect(menuBar).toHaveCSS('background-color', 'rgb(240, 240, 240)')
+        
+    })
+
+
+    test('verify User can see menu options on the main page', async ({ page }) => {
+
+        let array = [
+            "What's New",
+            'Women',
+            'Men',
+            'Gear',
+            'Training',
+            'Sale'
+        ];
+    
+        const menuOptions = await page.locator('.level-top.ui-corner-all');
+      
+    
+        for (let i = 0; i < menuOptions.length; i++) {
+            await expect(elements[i]).toBeVisible()
+            
+            const expectedLink = array[i];
+            const actualLink = await link.innerText();
+
+            await expect(menuOptions).toHaveCount(6);
+            await expect(actualLink).toEqual(expectedLink)
+        
+}        });
+    });
+    
+
+
+   
