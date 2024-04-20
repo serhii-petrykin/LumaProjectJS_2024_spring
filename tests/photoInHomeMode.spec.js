023c93169df4bb7cb13b332d2dc5523e79a33403
Yuliya Kuzz <jkuzmina84@gmail.com>
2024-04-20 09:20:16 +0400

    without timeout

--- tests/photoInHomeMode.spec.js
+++ tests/photoInHomeMode.spec.js
@@ -3,7 +3,6 @@ import {test, expect} from "@playwright/test"
 
 test.describe('photoInHomeMode', () => {
     test('photo in home mode', async ({page}) => {
-        test.setTimeout(60000);
         await page.goto('https://magento.softwaretestingboard.com/fusion-backpack.html');
         await expect(page.locator('.fotorama__stage .fotorama__active .fotorama__img')).toHaveAttribute("src", /gray/)
 
