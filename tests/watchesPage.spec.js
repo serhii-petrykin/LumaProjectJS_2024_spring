bf6b4b6704178998065aa2d2483c002b8b16a489
Sandarella <164547843+Sandarella@users.noreply.github.com>
2024-04-21 10:48:44 +0500

    TC 07.3.1_04 Gear/Watches. There is only a “Watches” on the open page (#207)

--- tests/watchesPage.spec.js
+++ tests/watchesPage.spec.js
@@ -114,5 +114,17 @@ test.describe('watchesPage', () => {
 
         expect(extractedGenders).toEqual(expectedGenderOptions);
     })
+
+    test ('There is only a “Watches” on the open page', async ({page}) => {
+        await page.locator('#ui-id-6').hover();
+        await page.locator('#ui-id-27').click();
+        const response = await page.request.get(baseURL + '/gear/watches.html?product_list_limit=24&product_list_mode=list');
+    
+        await expect(page.getByRole('heading', {name: 'Watches'})).toBeVisible();
+        const allTextItems = await page.locator('.products .product-items .product-item-link').allTextContents();
+        for (const item of allTextItems) {
+            expect(item).toContain('Watch');
+        }
+    })
     
 });
