48cc7515c3be4f8ec4539df1272d80c7dc81911f
Andrei Novikau <76206465+andreinovikau92@users.noreply.github.com>
2024-04-21 15:54:36 -0500

    TC 10.1.2_04 After entering a valid email to the Email field and clicking the 'Reset My Password’ button the 'Customer Login' page opens (#236)
    * After entering a valid email to the Email field and clicking the Reset My Password button the Customer Login page opens
    
    * After entering a valid email to the Email field and clicking the Reset My Password button the Customer Login page opens

--- tests/forgotPassword.spec.js
+++ tests/forgotPassword.spec.js
@@ -36,4 +36,20 @@ test.describe('Forgot Password Page', () => {
 
         await expect(page.getByRole('alert').first()).toHaveText(`If there is an account associated with ${email} you will receive an email with a link to reset your password.`);
     });
+    test('After entering a valid email to the Email field and clicking the Reset My Password button the Customer Login page opens', async({ page }) => {
+        function generateRandomEmail() {
+            const mailbox = Math.random().toString(36).substring(2, 10);
+            const domain = "example.com";
+            return `${mailbox}@${domain}`;
+          }
+
+        const email = generateRandomEmail();
+        await page.getByRole('link', {name: 'Sign In'}).click();
+        await page.getByRole('link', {name: 'Forgot Your Password?'}).click()
+        await page.locator('#email_address').fill(email);
+        await page.locator('.action.submit.primary').click();
+
+        await expect(page).toHaveURL('https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS9jdXN0b21lci9hY2NvdW50L2luZGV4Lw%2C%2C/');
+        await expect(page.locator('h1.page-title')).toHaveText('Customer Login');
+    });
 });
