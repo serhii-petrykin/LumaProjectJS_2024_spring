a1c63c86a2b9a9cb31f520a755acfabd0509961c
Alena <51464730+alenavolakhava@users.noreply.github.com>
2024-04-21 19:26:22 -0400

    gear: verify user can choose a bag by material (#173)

--- tests/gearBags.spec.js
+++ tests/gearBags.spec.js
@@ -1,14 +1,33 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("gearBags", () => {
+  let materialOption;
+
   test.beforeEach(async ({ page }) => {
     await page.goto("/");
+    materialOption = page.locator("text=Material");
   });
+
   test("Verify material sidebar menu option exists", async ({ page }) => {
     page.locator("#ui-id-6").hover();
     await page.locator("#ui-id-25").click();
     await expect(page).toHaveTitle("Bags - Gear");
-    const materialOption = page.locator("text=Material");
+
     await expect(materialOption).toBeVisible();
+  });
+
+  test("Verify user can choose a bag by material", async ({ page }) => {
+    page.locator("#ui-id-6").hover();
+    await page.locator("#ui-id-25").click();
+    await expect(page).toHaveTitle("Bags - Gear");
+    await materialOption.click();
+    const materialMenu = await page.locator(
+      "ol.items > li.item > a:has-text(' Polyester ')",
+      { timeout: 50000 } // Increase timeout value as needed
+    );
+    materialMenu.click();
+    await expect(
+      page.locator('.filter-value:has-text("Polyester")')
+    ).toBeVisible();
   });
 });
