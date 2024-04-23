d0fb3c5e2b4d7c0f511ae26dad4c02434daedc26
Elena Nov <93733106+ElNov686@users.noreply.github.com>
2024-04-23 19:32:18 +0100

    test verify 19 optins (#276)

--- tests/womenTopsStyle.spec.js
+++ tests/womenTopsStyle.spec.js
@@ -12,12 +12,39 @@ test.describe("womenTopsStyle", () => {
         await page.getByText("Women").hover();
         await page.getByRole('menuitem', { name: 'Tops' }).click();
         await expect(page).toHaveURL("https://magento.softwaretestingboard.com/women/tops-women.html");   
-    })
+    });
 
     test('Verify visability of Shopping Option in the menu on the left side.', async ({ page }) => {
         await page.getByText("Women").hover();
         await page.getByRole('menuitem', { name: 'Tops' }).click();
         await expect(page.getByRole('heading', { name: 'Shopping Options' })).toBeVisible();
         expect(page.getByRole('heading', { name: 'Shopping Options' })).toBeTruthy();
-    })
+    });
+
+    test('Verify dropdown menu has 19 Options', async ({ page }) => {
+        await page.getByText("Women").hover();
+        await page.getByRole('menuitem', { name: 'Tops' }).click();
+        await page.getByRole('tab', { name: 'Style' }).click();
+        
+        const expectedItems = ['Insulated', 'Jacket', 'Lightweight', 'Hooded', 'Heavy Duty', 'Rain Coat', 'Hard Shell', 'Soft Shell', 'Windbreaker', 
+            '¼ zip', 'Full Zip', 'Reversible', 'Bra', 'Sweatshirt', 'Tank', 'Tee', 'Pullover', 'Hoodie', 'Camisole'];
+            
+        const actualItems = await page.locator('#narrow-by-list').getByRole('list').locator('li').allInnerTexts();
+        function extractAndCompareItems(actualResult, expectedItems) {
+            const extractedItems = actualResult.map(item => {
+                return item.replace(/\nitem$/, '').split(' ').slice(0, -1).join(' ');
+            });
+            const areEqual = extractedItems.length === expectedItems.length && 
+                             extractedItems.every((item, index) => item === expectedItems[index]);
+        
+            return {
+                extractedItems,  
+                areEqual         
+            };
+         }
+          const result = extractAndCompareItems(actualItems, expectedItems);
+
+          expect(result.extractedItems).toEqual(expectedItems);
+        });
+    
 })
