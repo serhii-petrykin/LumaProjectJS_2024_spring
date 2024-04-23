e289bb41c993f9c83a14411d0945932cadf49702
ElenaDetusheva <148974974+ElenaDetusheva@users.noreply.github.com>
2024-04-23 18:20:02 +0300

    TC 11.4.4.02| Warning messages appear ,if "My Wish List" is empty. (#267)

--- tests/myWishList.spec.js
+++ tests/myWishList.spec.js
@@ -1,7 +1,6 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("MyWishList", () => {
-
   test.beforeEach(async ({ page }) => {
     await page.goto("/");
     const email = "rarara@gmail.com";
@@ -12,7 +11,7 @@ test.describe("MyWishList", () => {
     await page.getByRole("textbox", { name: "Password*" }).fill(password);
     await page.getByRole("button", { name: "Sign In" }).click();
   });
-  
+
   test("Customer Account/My Wish List", async ({ page }) => {
     const titleText = "My Wish List";
     const myWishListTitle = await page.locator("h1.page-title");
@@ -23,5 +22,15 @@ test.describe("MyWishList", () => {
     await expect(page.url()).toContain("/wishlist/");
     await expect(myWishListTitle).toBeVisible();
     await expect(myWishListTitle).toHaveText(titleText);
+  });
+
+  test("Warning Messages", async ({ page }) => {
+    await page.locator("div[class='panel header'] span[role='button']").click();
+    await page.getByRole("link", { name: "My Wish List" }).click();
+
+    for (const li of await page
+      .getByText("You have no items in your wish list.")
+      .all())
+      await expect(li).toBeVisible();
   });
 });
