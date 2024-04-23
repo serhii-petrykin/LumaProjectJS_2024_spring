3705f8a60748eea4777f1e9a0ea8a9a9093dc890
Serhii Petrykin <mistersergio777@gmail.com>
2024-04-23 19:29:13 +0300

    removing empty line

--- tests/logoIcon.spec.js
+++ tests/logoIcon.spec.js
@@ -24,6 +24,7 @@ test.describe("Check logo icon navigation", () => {
       await page.locator(".logo").click();
       await expect(page).toHaveURL("https://magento.softwaretestingboard.com");
     }
+    
     test('Reload the Home page when click on the logo', async({page})=> {
       await page.locator('.logo').click()
       await page.reload()
