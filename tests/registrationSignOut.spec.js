5e36c1b1e8d98075f1d512e7f47c3fa0e61b7c9b
Katerina Poliit <114866316+Katerina-Poliit@users.noreply.github.com>
2024-04-24 17:49:06 +0000

    TC 10.2.1_03 Checking drop-down menu should open (#308)

--- tests/registrationSignOut.spec.js
+++ tests/registrationSignOut.spec.js
@@ -2,8 +2,11 @@ import { test, expect } from "@playwright/test";
 
 test.describe('sign out', () => {
 
+    const email = 'katikati3@gmail.com';
+    const password = 'Password!';
 
   test.beforeEach(async ({ page }) => {
+
     await page.goto('/');
 
   })
@@ -19,6 +22,17 @@ test.describe('sign out', () => {
         await page.waitForTimeout(3000);
         await page.locator('body > div.page-wrapper > header > div.panel.wrapper > div > ul > li.greet.welcome > span');
         await expect(page.locator('body > div.page-wrapper > header > div.panel.wrapper > div > ul > li.greet.welcome > span')).toBeVisible();
+ })
+
+ test('drop-down menu should open', async ({ page }) => {
+
+    await page.getByRole('link', { name: 'Sign In' }).click();
+    await page.getByLabel('Email', { exact: true }).fill(email);
+    await page.getByLabel('Password').fill(password);
+    await page.getByRole('button', { name: 'Sign In' }).click();
+    await page.getByRole('banner').locator('button').filter({ hasText: 'Change' }).click();
+    await page.getByRole('banner').getByText('My Account My Wish List Sign');
+    await expect(page.getByRole('banner').getByText('My Account My Wish List Sign')).toBeTruthy();
  })
 
 })
