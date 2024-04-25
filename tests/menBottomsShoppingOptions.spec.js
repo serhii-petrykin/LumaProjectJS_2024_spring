e3c76fdeb135a39ea739fac908f7ce53f9196fab
Vera Harbachova <79797816+VeraHarbachova@users.noreply.github.com>
2024-04-25 09:55:24 +0200

    TC 16.1.1_02 Men/Bottoms/Shopping options/Category Verify the sidebar is on the left

--- tests/menBottomsShoppingOptions.spec.js
+++ tests/menBottomsShoppingOptions.spec.js
@@ -12,4 +12,17 @@ test.describe('MenBottomsShoppingOptions', () => {
 
         await expect(page).toHaveURL('https://magento.softwaretestingboard.com/men/bottoms-men.html');
     })    
+
+    test('verify the sidebar is on the left', async ({ page }) => {
+        await page.locator('#ui-id-5').hover();
+        await page.locator('#ui-id-18').click();
+
+        await expect(page.getByRole('heading', {name: 'Shopping Options'})).toBeVisible();
+       
+        const sidebarLeft = await page.$eval('.sidebar.sidebar-main', sidebar => {
+            return window.getComputedStyle(sidebar).float;
+          });
+
+        expect(sidebarLeft).toBe('left');
+    })
 })
