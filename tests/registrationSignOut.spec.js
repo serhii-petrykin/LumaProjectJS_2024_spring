9d499f2b0c240f4c99c750f3390c4185f2662f66
Katerina Poliit <114866316+Katerina-Poliit@users.noreply.github.com>
2024-04-25 09:09:28 +0000

    TC 10.2.1_04 Checking that there is a "Log out" link in the drop-down menu, the user logs out of his account when clicking (#337)

--- tests/registrationSignOut.spec.js
+++ tests/registrationSignOut.spec.js
@@ -35,4 +35,19 @@ test.describe('sign out', () => {
     await expect(page.getByRole('banner').getByText('My Account My Wish List Sign')).toBeTruthy();
  })
 
+ test('should be the "Log out" link, the user logs out of his account by clicking on it', async ({ page }) => {
+
+  await page.getByRole('link', { name: 'Sign In' }).click();
+  await page.getByLabel('Email', { exact: true }).fill(email);
+  await page.getByLabel('Password').fill(password);
+  await page.getByRole('button', { name: 'Sign In' }).click();
+
+  await page.waitForTimeout(3000);
+  await page.getByRole('banner').locator('button').filter({ hasText: 'Change' }).click();
+  await expect(page.getByRole('link', { name: 'Sign Out' })).toHaveCSS('cursor', 'pointer');
+  await page.getByRole('link', { name: 'Sign Out' }).click();
+  await expect(page).toHaveURL('https://magento.softwaretestingboard.com/customer/account/logoutSuccess/');
+
+ })
+
 })
