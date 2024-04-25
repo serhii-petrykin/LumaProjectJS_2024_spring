2029fd0116d9c785d19cbacfa6d3cd52c0ba4560
teya-teya <142177100+teya-teya@users.noreply.github.com>
2024-04-25 14:56:59 +0300

    TC 14.1.2_05 Women/Bottoms/Shopping options/Style (#323)

--- tests/womenBottomStyle.spec.js
+++ tests/womenBottomStyle.spec.js
@@ -46,4 +46,32 @@ test.describe('Women/Bottoms/Style', () => {
     await expect(womenMenu).toHaveCSS('border-style', 'solid');
     await expect(womenMenu).toHaveCSS('color', 'rgb(51, 51, 51)');
   });
+
+  test('Verify the availability of a list of 9 category in the "Style" option drop-down list', async ({ page }) => {
+    const womenMenu = page.locator("a#ui-id-4");
+    await womenMenu.hover();
+    await page.getByRole("menuitem", { name: "Bottoms" }).click();
+
+    await expect(page).toHaveURL("https://magento.softwaretestingboard.com/women/bottoms-women.html");
+
+    const styleDrop = page.getByRole("tab", { name: "Style" });
+    await styleDrop.click();
+
+    expect(styleDrop.locator("[aria-selected]")).toBeTruthy();
+
+    const expectedItems = ['Base Layer', 'Basic', 'Capri', 'Compression', 'Leggings', 'Parachute', 'Snug', 'Sweatpants', 'Track Pants'];
+    const receivedResult = await page.locator('[aria-hidden="false"] .items > .item').allInnerTexts();
+
+    function extractAndCompareItems(receivedResult, expectedItems) {
+      const extractedItems = receivedResult.map(item => item.replace(/\nitem$/, '').split(' ').slice(0, -1).join(' '));
+      const areEqual = JSON.stringify(extractedItems) === JSON.stringify(expectedItems);
+
+      return { extractedItems, areEqual };
+    }
+
+    const result = extractAndCompareItems(receivedResult, expectedItems);
+
+    expect(result.extractedItems).toEqual(expectedItems);
+
+  });
 });
