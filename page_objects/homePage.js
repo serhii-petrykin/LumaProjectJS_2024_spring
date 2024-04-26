4c198075e6c7a3f968dd384ea47029941e56471f
Natalia Vasylieva <123753026+NatalyaVasylyeva@users.noreply.github.com>
2024-04-26 17:56:39 +0200

    RF_POM | header.spec | TC 01.2.1_09 Verify the automatic search results match the query in the search bar (#355)

--- page_objects/homePage.js
+++ page_objects/homePage.js
@@ -3,36 +3,59 @@ import WomenPage from "./womenPage.js";
 import MenPage from "./menPage";
 
 class HomePage {
-    constructor(page) {
-        this.page = page
-    }
+  constructor(page) {
+    this.page = page;
+  }
 
-    locators = {
-        getWhatsNewLink: () => this.page.getByRole('listitem').filter({ hasText: "What's New" }),
-        getWomenLink: () => this.page.locator(".nav-sections .navigation li a[href$='/women.html']"),
-        getMenLink: () => this.page.getByRole('menuitem', {name: 'Men'}).last()
-    }
+  locators = {
+    getWhatsNewLink: () =>
+      this.page.getByRole("listitem").filter({ hasText: "What's New" }),
+    getWomenLink: () =>
+      this.page.locator(".nav-sections .navigation li a[href$='/women.html']"),
+    getMenLink: () => this.page.getByRole('menuitem', {name: 'Men'}).last(),
+    getSearchInputField: () =>
+      this.page.getByPlaceholder("Search entire store here..."),
+    getWaitForAutocompleteSearchItems: () =>
+      this.page.waitForSelector("#search_autocomplete>ul>li>span:first-child"),
+    getAutocompleteSearchItems: () =>
+      this.page.locator("#search_autocomplete>ul>li>span:first-child"),
+  };
 
-    async open() {
-        await this.page.goto('/')
-    }
+  async open() {
+    await this.page.goto("/");
+  }
 
-    async clickWhatsNewLink() {
-        await this.locators.getWhatsNewLink().click();
+  async clickWhatsNewLink() {
+    await this.locators.getWhatsNewLink().click();
 
-        return new WhatsNewPage(this.page);
-    }
+    return new WhatsNewPage(this.page);
+  }
 
-    async clickWomenLink() {
-        await this.locators.getWomenLink().click();
+  async clickWomenLink() {
+    await this.locators.getWomenLink().click();
 
-        return new WomenPage(this.page);
-    }
+    return new WomenPage(this.page);
+  }
 
     async clickMenLink() {
         await this.locators.getMenLink().click();
 
         return new MenPage(this.page);
     }
+
+  async fillSearchInputField(searchQuerry) {
+    await this.locators.getSearchInputField().fill(searchQuerry);
+
+    return this;
+  }
+
+  async executeSearchAutocompleteList() {
+    await this.locators.getWaitForAutocompleteSearchItems();
+    const searchAutocompleteList = await this.locators
+      .getAutocompleteSearchItems()
+      .allInnerTexts();
+
+    return searchAutocompleteList;
+  }
 }
 export default HomePage;
--- tests/test_page_objects/homePage.spec.js
+++ tests/test_page_objects/homePage.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 import HomePage from "../../page_objects/homePage.js";
-import { BASE_URL, WHATS_NEW_PAGE_END_POINT, WHATS_NEW_PAGE_HEADER } from "../../helpers/testData.js";
+import { BASE_URL, WHATS_NEW_PAGE_END_POINT, WHATS_NEW_PAGE_HEADER, SEARCH_QUERY } from "../../helpers/testData.js";
 
 test.describe('homePage.spec', () => {
     test('verify user can navigate to home page clicking on logo from "What\'s New" page', async ({ page }) => {
@@ -14,4 +14,17 @@ test.describe('homePage.spec', () => {
         await whatsNewPage.clickLogoLink()
         await expect(page).toHaveURL(BASE_URL);
     })
+
+    test('Verify  the automatic search results match the query in the search bar', async ({
+        page,
+    }) => {
+        const homePage = new HomePage(page);
+
+        await homePage.open();
+        await homePage.fillSearchInputField(SEARCH_QUERY);
+
+        expect(await homePage.executeSearchAutocompleteList()).toContain(
+            SEARCH_QUERY
+        );
+    });
 })
