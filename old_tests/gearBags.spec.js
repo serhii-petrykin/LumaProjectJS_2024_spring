05c659430522ada7223f599adf023a817d04e598
Anastasiia Go <132062068+Anastassyia@users.noreply.github.com>
2024-04-29 20:47:44 +0300

    TC 07.1.4_01 Verify bags display mode (#402)
    https://trello.com/c/L1XN0G80

--- old_tests/gearBags.spec.js
+++ old_tests/gearBags.spec.js
@@ -119,5 +119,18 @@ test.describe("gearBags", () => {
       await page.goto('https://magento.softwaretestingboard.com/gear/bags.html?material=35');
     }    
   })
+  test ('Verify bags display mode', async({ page }) => {
+    test.setTimeout(60*1000)
+    await page.locator('#ui-id-6').hover();
+    await page.locator('#ui-id-25').click();
+    await expect(page).toHaveURL('/gear/bags.html');
+    await expect(page).toHaveTitle('Bags - Gear');
+
+    await page.getByRole('link', { name: 'View as  List' }).click();
+
+    await expect(page).toHaveURL('/gear/bags.html?product_list_mode=list');
+    await expect(page.locator('//select[@id="limiter"]//option[2]').last()).toHaveAttribute('selected');
+    await expect(page.locator('div.toolbar-products [title="List"]').first()).toHaveAttribute('class', 'modes-mode active mode-list');
+})
 
 });
