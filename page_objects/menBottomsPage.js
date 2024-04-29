a2e540cda7918a7db91530c7d1e2587c19aecd8f
Vera Harbachova <79797816+VeraHarbachova@users.noreply.github.com>
2024-04-29 22:53:48 +0200

    RF_POM | menBottomsPage.spec  | TC 16.1.1_02 Men/Bottoms/Shopping options/Category Verify the sidebar is on the left (#408)

--- page_objects/menBottomsPage.js
+++ page_objects/menBottomsPage.js
@@ -11,13 +11,23 @@ class MenBottomsPage {
         getBreadcrumbsMenuMen: () => this.page.locator('[class="item category11"]'),
         getBreadcrumbsMenuBottoms: () => this.page.locator('[class="item category13"]'),
         breadcrumbsMenuMen: () => this.page.locator(
-            'xpath=//li[@class="item category11"]/a[@href="https://magento.softwaretestingboard.com/men.html"]')
+            'xpath=//li[@class="item category11"]/a[@href="https://magento.softwaretestingboard.com/men.html"]'),
+        getMenBottomsShopingOptionsSidebarTitle: () => this.page.getByRole('heading', {name: 'Shopping Options'}),
+        getMenBottomsShopingOptionsSidebarPosition: () => this.page.locator('.sidebar.sidebar-main'),
     }
 
     async clickBreadcrumbsMenuMen() {
         await this.locators.breadcrumbsMenuMen().click();
 
         return new MenPage(this.page);
+    }
+
+    async getPositionOfSidebar() {
+        const position = await this.page.$eval('.sidebar.sidebar-main', sidebar => {
+            return window.getComputedStyle(sidebar).float;
+          });
+
+          return position;
     }
 }
 export default MenBottomsPage;
--- tests/test_page_objects/menBottomsPage.spec.js
+++ tests/test_page_objects/menBottomsPage.spec.js
@@ -4,18 +4,41 @@ import HomePage from "../../page_objects/homePage.js";
 import MenBottomsPage from "../../page_objects/menBottomsPage.js";
 import { BASE_URL, MEN_BOTTOMS_PAGE_END_POINT } from "../../helpers/testData.js";
 
-    test.describe ('menBottomsPage.spec', () => {
+  test.describe ('menBottomsPage.spec', () => {
+    test.beforeEach(async({page}) => {
+      const homePage = new HomePage(page);
+
+      await homePage.open();
+
+      if (await page.getByRole('dialog', { name: 'This site asks for consent to use your data' }).isVisible()) {
+        await page.getByRole('button', { name: 'Consent' }).click();
+    };
+  })
+
     test ("Verify men's bottom tab", async ({ page }) => {
 
     const homePage = new HomePage(page);
     const menBottomsPage = new MenBottomsPage(page);
 
-    await homePage.open();
     await homePage.hoverMenLink();
     await homePage.clickMenBottomsLink();
 
     await expect(page).toHaveURL(BASE_URL + MEN_BOTTOMS_PAGE_END_POINT);
     await expect(menBottomsPage.locators.getBottomsHeading()).toBeVisible();
   });
-  });
+
+  test('verify the sidebar is on the left', async ({ page }) => {
+    const homePage = new HomePage(page);
+    const menBottomsPage = new MenBottomsPage(page);
+
+    await homePage.hoverMenLink();
+    await homePage.clickMenBottomsLink();
+
+    await expect(menBottomsPage.locators.getMenBottomsShopingOptionsSidebarTitle()).toBeVisible();
+   
+    const positionOfSidebar = await menBottomsPage.getPositionOfSidebar();
+
+    expect(positionOfSidebar).toBe('left');
+  })
+});
   
