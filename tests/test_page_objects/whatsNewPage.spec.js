59698516d5115a3951bb01ff3e96148b9d84a81c
Halyna A <125617798+7108581@users.noreply.github.com>
2024-04-29 11:04:42 -0700

    RF_POM |whatsNewPage.spec.js | TC 04.1.1_01 Verify that the user is redirected to the "What's New" page by clicking on the Menu item "What's New" (#401)

---
+++ tests/test_page_objects/whatsNewPage.spec.js
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+import HomePage from '../../page_objects/homePage.js';
+import WhatsNewPage from '../../page_objects/whatsNewPage.js';
+import { BASE_URL,WHATS_NEW_PAGE_END_POINT, WHATS_NEW_PAGE_HEADER } from '../../helpers/testData.js';
+
+test.describe('whatIsNewPage.spec', () => {
+    test.beforeEach(async({page}) => {
+        const homePage = new HomePage(page);
+
+        await homePage.open();
+    })
+
+     test('Redirect to "Whats New" page', async({page}) => {
+        const homePage = new HomePage(page);
+        const whatsNewPage = new WhatsNewPage(page);
+
+        await homePage.clickWhatsNewLink();
+       
+        await expect(page).toHaveURL(BASE_URL + WHATS_NEW_PAGE_END_POINT);
+        await expect(whatsNewPage.locators.getPageHeader()).toHaveText(WHATS_NEW_PAGE_HEADER);
+    });  
+})
